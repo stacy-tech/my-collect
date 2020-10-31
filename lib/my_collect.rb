6991dde6b521c52fd9ae20135c72b7f13de9e6b6
stacy <stacydn23@gmail.com>
2020-10-30 22:54:16 -0400

    commit tree cbcb996ca3d69b19c3deb8658607e48db31acee7

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+    i = 0
+    result = []
+    while i < array.size
+        block_given?
+        result << yield(array[i])
+        i += 1
+    end
+    result
+end
+
+  
 
 
+    
